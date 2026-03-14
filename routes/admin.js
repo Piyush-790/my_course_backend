7ad38c5a5df751cb97e3eef9657e148634776374
Piyush Kumar <piyushkumar.5521@gmail.com>
2026-03-14 06:04:53 +0530

    added the adin route

---
+++ routes/admin.js
@@ -0,0 +1,35 @@
+const {Router} = require('express');
+const adminRouter = Router();
+
+
+adminRouter.post('/dashboard',(req,res)=>{
+    res.json({
+        message:"Admin dashboard"
+    })
+})
+
+adminRouter.post('/login',(req,res)=>{
+    res.json({
+        message:"Admin login successful"
+    })
+})
+
+adminRouter.post('/signup',(req,res)=>{
+    res.json({
+        message:"Admin signup successful"
+    })
+})
+
+adminRouter.post('/course',(req,res)=>{
+    res.json({
+        message:"Admin created successfully"
+    })
+})
+
+adminRouter.put('/course',(req,res)=>{
+    res.json({
+        message:"Admin updated successfully"
+    })
+})
+
+module.exports= {adminRouter:adminRouter};
